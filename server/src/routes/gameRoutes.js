d5dfdee76a77f2166a0a4d2cfe9b98159e830b3e
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-04-20 23:13:24 +0200

    feat: Implement full game CRUD routes

--- server/src/routes/gameRoutes.js
+++ server/src/routes/gameRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { getAllGames, getGameById } = require("../controllers/gameController");
+
+const {
+  getAllGames,
+  getGameById,
+  createGame,
+  updateGame,
+  deleteGame,
+} = require("../controllers/gameController");
 
 router.get("/", getAllGames);
 router.get("/:id", getGameById);
+router.post("/", createGame);
+router.patch("/:id", updateGame);
+router.delete("/:id", deleteGame);
 
 module.exports = router;
